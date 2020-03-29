d6fd641e9de32c9dcb4e23b8483b6a66bb581b2f
VittoFab <vittorio.fabris@mail.polimi.it>
2020-03-29 13:50:19 +0200

    class map and cell 1

--- src/main/java/it/polimi/ingsw/model/Cell.java
+++ src/main/java/it/polimi/ingsw/model/Cell.java
@@ -1,4 +1,65 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
+/**
+ * This class represents one map's cell structure
+ */
+
 public class Cell {
+
+    private Map map;
+    private boolean dome;
+    private int level;
+    private ArrayList<Worker> workers;
+    private int x;
+    private int y;
+    private final boolean perimetral;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.level = 0;
+        this.workers = null;
+        this.dome = false;
+        if (x == 0 || x == (map.getBorderLength()-1) || y == 0 || y == (map.getBorderLength()-1))
+            perimetral = true;
+        else
+            perimetral = false;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean getDome() {
+        return dome;
+    }
+
+    public void setDome(boolean dome) {
+        this.dome = dome;
+    }
+
+    /**
+     * Tells if in this cell there's a worker or not
+     * @return  true if the cell contains a worker
+     */
+    public boolean hasWorker() {
+        if (workers.size()>0)
+            return true;
+        return false;
+    }
+
+    /**
+     * States if the cell stays on the border of the map
+     * @return  true if the cell is on the border of the map
+     */
+    public boolean isPerimetral() {
+        return perimetral;
+    }
+
 }
--- src/main/java/it/polimi/ingsw/model/Map.java
+++ src/main/java/it/polimi/ingsw/model/Map.java
@@ -1,4 +1,39 @@
 package it.polimi.ingsw.model;
 
+/**
+ * This class represents the structure where the players can play their game
+ */
+
 public class Map {
+    private Game game;
+    private Cell[][] map;
+    private final int borderLength = 5;
+
+    public Map(Game game) {
+        this.game = game;
+        this.map = new Cell[borderLength][borderLength];
+        for(int i=0; i<borderLength; i++){
+            for(int j=0; j<borderLength; j++){
+                map[i][j] = new Cell(i, j);
+            }
+        }
+    }
+
+    public int getBorderLength(){
+        return borderLength;
+    }
+
+    public Cell[][] getMap(){
+        return map;
+    }
+
+    /**
+     * used to find one specific cell on the map
+     * @param x row of the map
+     * @param y column of the map
+     * @return  cell which I specified with the parameters
+     */
+    public Cell findCell(int x, int y) {
+        return map[x][y];
+    }
 }
