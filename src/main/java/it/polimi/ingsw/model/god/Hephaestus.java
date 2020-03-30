687b842de25ff9d11bc0c08dd2120ace320d23c8
AlbertoLatino <alberto.latino@mail.polimi.it>
2020-03-30 20:02:06 +0200

    Created Hephaestus class and its secondBuild code. Changed the return value of build in God: now returns Cell.

--- src/main/java/it/polimi/ingsw/model/god/Hephaestus.java
+++ src/main/java/it/polimi/ingsw/model/god/Hephaestus.java
@@ -9,6 +9,10 @@ public class Hephaestus implements God{
 
     Cell firstBuildingCell;
 
+    /**
+     * This method calls the sequence of actions that can be done by the player who owns Hephaestus.
+     * @param w This is the current worker.
+     */
     public void evolveTurn(Worker w) {
         move(w);
         win(w);
@@ -16,7 +20,10 @@ public class Hephaestus implements God{
         secondBuild(w);
     }
 
-
+    /**
+     * This method allows the player to build in the same place twice.
+     * @param w This is the player's current worker.
+     */
     public void secondBuild(Worker w){
 
         Scanner input = new Scanner(System.in);
@@ -29,7 +36,7 @@ public class Hephaestus implements God{
 
             if (command.equals("Yes")) {
 
-                if(firstBuildingCell.getLevel()<3 && !(firstBuildingCell.hasDome()){
+                if(firstBuildingCell.getLevel()<3 && !(firstBuildingCell.hasDome())){
 
                     w.buildBlock(firstBuildingCell.getX(),firstBuildingCell.getY());
                     break;
