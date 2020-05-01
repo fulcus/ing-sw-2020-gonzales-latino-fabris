13f888d5c6ad49d504f0ffa232042b3fd713107a
VittoFab <vittorio.fabris@mail.polimi.it>
2020-05-02 00:11:08 +0200

    Created GodControllerTest and GameControllerTest classes

---
+++ src/test/java/it/polimi/ingsw/server/controller/GodControllerTest.java
@@ -0,0 +1,84 @@
+package it.polimi.ingsw.server.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GodControllerTest {
+
+    @Test
+    public void updateCurrentClient() {
+    }
+
+    @Test
+    public void getInputInCoordinates() {
+    }
+
+    @Test
+    public void getInputMove() {
+    }
+
+    @Test
+    public void wantToMoveAgain() {
+    }
+
+    @Test
+    public void wantToMoveUp() {
+    }
+
+    @Test
+    public void wantToMoveEnemy() {
+    }
+
+    @Test
+    public void forceMoveEnemy() {
+    }
+
+    @Test
+    public void displayBoard() {
+    }
+
+    @Test
+    public void getBuildingInputAtlas() {
+    }
+
+    @Test
+    public void getBuildingInput() {
+    }
+
+    @Test
+    public void wantToBuildAgain() {
+    }
+
+    @Test
+    public void allowBuildUnderneath() {
+    }
+
+    @Test
+    public void winGame() {
+    }
+
+    @Test
+    public void errorMoveScreen() {
+    }
+
+    @Test
+    public void errorMoveDecisionScreen() {
+    }
+
+    @Test
+    public void errorBuildDecisionScreen() {
+    }
+
+    @Test
+    public void errorBuildInSamePosition() {
+    }
+
+    @Test
+    public void errorBuildScreen() {
+    }
+
+    @Test
+    public void errorBuildBlockScreen() {
+    }
+}
