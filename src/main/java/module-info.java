e0186b1828d891c10b156c5c652f2d44abc19978
VittoFab <vittorio.fabris@mail.polimi.it>
2020-05-07 18:13:58 +0200

    Completed GodControllerTest testing class.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module it.polimi.ingsw.ps57 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    // Se usate GSON aggiungere anche:
+    // requires java.sql;
+}
