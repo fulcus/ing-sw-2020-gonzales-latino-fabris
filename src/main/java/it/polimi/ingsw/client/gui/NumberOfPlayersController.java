68d572aa91e09e39b8d921601e726de11d2500b5
VittoFab <vittorio.fabris@mail.polimi.it>
2020-05-11 21:48:25 +0200

    HestiaTest completed. Implemented first choosing number of players scene for the gui

---
+++ src/main/java/it/polimi/ingsw/client/gui/NumberOfPlayersController.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.client.gui;
+
+import javafx.fxml.FXML;
+
+public class NumberOfPlayersController {
+
+    public NumberOfPlayersController() {
+    }
+
+    @FXML
+    private void two() {
+        System.out.println("two");
+    }
+
+    @FXML
+    private void three() {
+        System.out.println("three");
+    }
+
+    @FXML
+    private void next() {
+        System.out.println("next scene");
+    }
+}
+
+
