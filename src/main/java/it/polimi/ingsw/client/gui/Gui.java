30358fe9902b09d98fffd0b253fd754845d6f0b5
fulcus <francescofgonzales@gmail.com>
2020-05-16 00:33:13 +0200

    Created win and lose scenes Edited all css files to load font from resources

--- src/main/java/it/polimi/ingsw/client/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/client/gui/Gui.java
@@ -17,9 +17,9 @@ public class Gui extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            //Parent root = FXMLLoader.load(getClass().getResource("/scenes/welcome.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/scenes/welcome.fxml"));
             //Parent root = FXMLLoader.load(getClass().getResource("/scenes/connect.fxml"));
-            Parent root = FXMLLoader.load(getClass().getResource("/scenes/choose-num-of-players.fxml"));
+            //Parent root = FXMLLoader.load(getClass().getResource("/scenes/choose-num-of-players.fxml"));
             //Parent root = FXMLLoader.load(getClass().getResource("/scenes/choose-nickname.fxml"));
             //Parent root = FXMLLoader.load(getClass().getResource("/scenes/choose-color.fxml"));
             //Parent root = FXMLLoader.load(getClass().getResource("/scenes/start-player.fxml"));
