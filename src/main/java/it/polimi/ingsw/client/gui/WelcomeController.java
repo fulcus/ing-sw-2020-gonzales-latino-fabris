439aa759f67cc0ba92c6ea874b340796cb527c9c
VittoFab <vittorio.fabris@mail.polimi.it>
2020-05-29 17:34:45 +0200

    Modified width and height of initial scenes and their customization created glow effect for the welcome scene

--- src/main/java/it/polimi/ingsw/client/gui/WelcomeController.java
+++ src/main/java/it/polimi/ingsw/client/gui/WelcomeController.java
@@ -2,10 +2,15 @@ package it.polimi.ingsw.client.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.Glow;
 
 import static it.polimi.ingsw.client.gui.GuiManager.connectRoot;
 
 public class WelcomeController {
+
+    @FXML
+    private Button start;
 
     public WelcomeController() {
     }
@@ -13,6 +18,20 @@ public class WelcomeController {
     @FXML
     private void next() {
         Gui.getStage().setScene(new Scene(connectRoot));
+    }
+
+    @FXML
+    private void glow() {
+        Glow glow = new Glow();
+        glow.setLevel(3);
+        start.setEffect(glow);
+    }
+
+    @FXML
+    private void normal() {
+        Glow glow = new Glow();
+        glow.setLevel(0);
+        start.setEffect(glow);
     }
 
 }
